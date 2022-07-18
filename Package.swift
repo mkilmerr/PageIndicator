36210fb44be5694c8fafb2fb761617fb819b19e6
mkilmerr <marcoskilmer1@gmail.com>
2022-07-18 09:43:35 -0300

    Array of platforms fixed

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PageIndicator",
-    platforms: [.iOS(.v15), .iOS(.v14), .iOS(.v13), .iOS(.v12)],
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
